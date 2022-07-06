8c149bf01aca6ff11ce86508d62a91f49db235f2
Nate Shuster <shusternathaniel@gmail.com>
2022-07-05 22:04:21 -0400

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,28 @@
+const move = ["rock", "paper", "scissors"];
+let playerSelection = "paper";
+let computerSelection = "";
+
 function computerPlay() {
-    let move = ["rock", "paper", "scissors"]
-    return move[Math.floor(Math.random() * move.length)]
+    computerSelection = move[Math.floor(Math.random() * move.length)];
+    return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let comp = computerSelection
+    return determineWinner(player, comp)
+}
+
+function determineWinner(player, computer) {
+    if (player === computer) {
+        return "draw.";
+    } else if (player === "rock" && computer === "paper") {
+        return "you lose.";
+    } else if (player === "paper" && computer === "scissors") {
+        return "you lose.";
+    } else if (player === "scissors" && computer === "rock") {
+        return "you lose.";
+    } else {
+        return "you win."
+    }
 }
