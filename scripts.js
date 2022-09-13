d3fc5e8b55477fa8cb91db73ccfa7f5b3e5d47a7
Nate Shuster <shusternathaniel@gmail.com>
2022-09-13 16:56:52 -0400

    Added update record function to record score

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 const move = ["rock", "paper", "scissors"];
+let record = {
+    wins: 0,
+    losses: 0
+}
 
 function computerPlay() {
     return move[Math.floor(Math.random() * move.length)];
@@ -12,16 +16,16 @@ function displayWinner(player, computer) {
         return "draw";
     } else if (player === move[0] && computer === move[1]) {
         resultPara.textContent = 'result: loss.'
-        return "computer";
+        return "losses";
     } else if (player === move[1] && computer === move[2]) {
         resultPara.textContent = 'result: loss.'
-        return "computer";
+        return "losses";
     } else if (player === move[2] && computer === move[0]) {
         resultPara.textContent = 'result: loss.'
-        return "computer";
+        return "losses";
     } else {
         resultPara.textContent = 'result: win.'
-        return "player";
+        return "wins";
     }
 }
 
@@ -32,18 +36,28 @@ function displayChoices(player, computer) {
     compPara.textContent = `computer played: ${computer}`;
 }
 
+function updateRecord(outcome, record) {
+    let recordPara = document.querySelector('.player-record');
+
+    if (outcome === "wins") {
+        record[outcome]++
+        recordPara.textContent = `record: ${record["wins"]}-${record["losses"]}`;
+    } else if (outcome === "losses") {
+        record[outcome]++
+        recordPara.textContent = `record: ${record["wins"]}-${record["losses"]}`;
+    } else {
+        return;
+    }
+}
+
 function playRound(e) {
     let player = e.target.id;
     let computer = computerPlay();
     displayChoices(player, computer);
-    displayWinner(player, computer);
-
     let winner = displayWinner(player, computer);
 
-    let record = {
-        wins: 0,
-        losses: 0
-    }
+    updateRecord(winner, record);
+    updateMatchResult();
 }
 
 const buttons = document.querySelectorAll('button');
