117541bd9748f7a2a497f1cc8ead496692bd622b
Nate Shuster <shusternathaniel@gmail.com>
2022-09-13 12:22:50 -0400

    Add display choices and refactor display winners functions

--- scripts.js
+++ scripts.js
@@ -4,30 +4,46 @@ function computerPlay() {
     return move[Math.floor(Math.random() * move.length)];
 }
 
-function roundResult(player, computer) {
+function displayWinner(player, computer) {
     let resultPara = document.querySelector('.round-result');
 
     if (player === computer) {
         resultPara.textContent = 'result: draw.';
-    } else if (player === "rock" && computer === "paper") {
+        return "draw";
+    } else if (player === move[0] && computer === move[1]) {
         resultPara.textContent = 'result: loss.'
-    } else if (player === "paper" && computer === "scissors") {
+        return "computer";
+    } else if (player === move[1] && computer === move[2]) {
         resultPara.textContent = 'result: loss.'
-    } else if (player === "scissors" && computer === "rock") {
+        return "computer";
+    } else if (player === move[2] && computer === move[0]) {
         resultPara.textContent = 'result: loss.'
+        return "computer";
     } else {
         resultPara.textContent = 'result: win.'
+        return "player";
     }
+}
+
+function displayChoices(player, computer) {
+    let playerPara = document.querySelector('.player-choice');
+    playerPara.textContent = `you played: ${player}`;
+    let compPara = document.querySelector('.computer-choice');
+    compPara.textContent = `computer played: ${computer}`;
 }
 
 function playRound(e) {
     let player = e.target.id;
     let computer = computerPlay();
-    let playerPara = document.querySelector('.player-choice')
-    playerPara.textContent = `you played: ${player}`
-    let aiPara = document.querySelector('.computer-choice')
-    aiPara.textContent = `computer played: ${computer}`
-    return roundResult(player, computer);
+    displayChoices(player, computer);
+    displayWinner(player, computer);
+
+    let winner = displayWinner(player, computer);
+
+    let record = {
+        wins: 0,
+        losses: 0
+    }
 }
 
 const buttons = document.querySelectorAll('button');
